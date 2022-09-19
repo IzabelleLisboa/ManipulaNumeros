85f3ac198fe5b61ba9e7789a8d3d9b2db5080d3b
Izabelle Lisboa <izabellelimalisboa10@gmail.com>
2022-09-19 15:46:08 +0100

    ManipulaNumeros part 1

---
+++ ManipulaNumeros/src/manipulanumeros/MaiorNumero.java
@@ -0,0 +1,17 @@
+
+package manipulanumeros;
+
+
+public class MaiorNumero {
+
+    static void maior2(int x, int y) {
+        if(x>y)
+            System.out.println(x+"Maior que "+y);
+        else if (y>x)
+            System.out.println(y+"Igual a "+x);
+        else
+            System.out.println(x+"Igual a "+y);
+        
+    }
+    
+}
